82f28e93ca308f11e60d5052782686e20b82f75b
vidaxd <>
2019-01-31 18:00:42 +0000

    accum method developed, test passed

--- src/main/java/es/ulpgc/Kata.java
+++ src/main/java/es/ulpgc/Kata.java
@@ -2,6 +2,21 @@ package es.ulpgc;
 
 public class Kata {
     public static String accum(String chain) {
-        return "";
+
+        if (chain.isEmpty()) return "";
+
+        String tranformedChain = "";
+
+        for (int i = 0; i < chain.length(); i++) {
+
+            tranformedChain += Character.toUpperCase(chain.charAt(i));
+
+            for (int j = 0; j < i; j++) {
+                tranformedChain += Character.toLowerCase(chain.charAt(i));
+            }
+
+            if (i < chain.length()-1) tranformedChain += "-";
+        }
+        return tranformedChain;
     }
 }
